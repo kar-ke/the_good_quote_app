817146aa815769d9a4f3ecec0d5593418795fcca
Karthikeyan Ramesh <karthikramesh444@gmail.com>
2024-03-24 13:37:05 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+	const [quote, setQuote] = useState("Hello world");
+
+	const fetchQuote = async () => {
+		try {
+			let response = await fetch("https://api.adviceslip.com/advice");
+			let data = await response.json();
+			setQuote(data.slip.advice);
+		} catch (error) {
+			console.error("Error fetching data:", error);
+		}
+	};
+
+	const handleClick = () => {
+		fetchQuote();
+	};
+
+	return (
+		<div className="app">
+			<div className="logocontainer">
+				<img src="../src/assets/quotefevicon.png" className="w-16" />
+				<h1 className="text-3xl text-white">The Good Quote</h1>
+			</div>
+			<div className="card mb-32">
+				<p className="quote">{quote}</p>
+				<button onClick={handleClick} className="button">
+					<span>give me advice</span>
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default App;
